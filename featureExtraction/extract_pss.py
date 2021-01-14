8597294a7dc2b52663a19babb5399cef254f918a
Sharmi <shtaaa@gmail.com>
2021-01-14 15:43:37 -0800

    updates

--- featureExtraction/extract_pss.py
+++ featureExtraction/extract_pss.py
@@ -1,4 +1,4 @@
-from featureExtraction.utils import pss_extraction_utils
+from featureExtraction.utils import pss_extraction_utils_updated
 import math
 from analysisdatalink.datalink_ext import AnalysisDataLinkExt as AnalysisDataLink
 import importlib
@@ -17,6 +17,7 @@ import glob
 import umap
 import matplotlib.pyplot as plt
 from sklearn import datasets, decomposition, manifold, preprocessing
+import cloudvolume
  
 from featureExtraction import provider
 import importlib
@@ -52,152 +53,135 @@ from functools import partial
 from scipy import sparse
 from meshparty import skeleton_io
 from analysisdatalink.datalink_ext import AnalysisDataLinkExt as AnalysisDataLink
-from annotationframeworkclient import infoservice
+from annotationframeworkclient import infoservice,FrameworkClient
 
 
-
-def extract_shape(cfg,cell_id):
-
+def extract_shape_updated(cfg,cell_id,run):
     procObj = {}
     
     #from cfg
+    procObj['synapse_scale'] = cfg['synapse_scale']
     procObj['local_dist_thresh'] = cfg['local_dist_thresh']
     procObj['cgal_number_of_rays'] = cfg['cgal_number_of_rays']
     procObj['cgal_number_of_clusters'] = cfg['cgal_number_of_clusters']
     procObj['cgal_smoothing_lambda'] = cfg['cgal_smoothing_lambda']
     procObj['cgal_cone_angle'] = cfg['cgal_cone_angle']
     procObj['pointnet_GPU_INDEX'] = cfg['pointnet_GPU_INDEX']
+    procObj['segsource'] = cfg['segsource']
+    procObj['token'] = cfg['token']
+    procObj['type_of_shape'] = cfg['type_of_shape']
     
     #inits - just cell id and directories
     procObj['cell_id'] = int(cell_id)
     procObj['myfiles'] =  glob.glob(cfg['pss_directory']+'%s/PSS*.txt'%cell_id)
     procObj['offfiles'] =  glob.glob(cfg['pss_directory']+'%s/PSS*.off'%cell_id)
-    procObj['outdir'] = cfg['pss_directory']+'/%s'%str(cell_id)
+    procObj['outdir'] = cfg['pss_directory']+'/' + cfg["type_of_shape"]+'/%s'%str(cell_id)
+    
     if not os.path.exists(procObj['outdir']):
             os.makedirs(procObj['outdir'])
 
-    #download mesh,skeleton and synapses and center of mass and set values
-    mm=trimesh_io.MeshMeta(cv_path=cfg['cv_path'], disk_cache_path=procObj['outdir'])
-    procObj['d_mesh'] = mm.mesh(seg_id=int(cell_id), merge_large_components=False)
-    skelfile = procObj['outdir']+'/%s_skeleton.h5'%int(cell_id)
-    if not os.path.exists(skelfile):
-        procObj['sk'] = skeletonize.skeletonize_mesh(procObj['d_mesh'],verbose=False)
-        skeleton_io.write_skeleton_h5(procObj['sk'], skelfile)
+    procObj['d_mesh'] = None
+    procObj['sk'] = None
+    print("This is my cell id: ",cell_id)
+    if cfg["type_of_shape"] == "postsynaptic":
+        procObj['data_synapses']= cfg['client'].materialize.query_table('synapses_pni_2',filter_in_dict={'post_pt_root_id':['%d'%cell_id]})
+        
+    elif cfg["type_of_shape"] == "presynaptic":
+        procObj['data_synapses']= cfg['client'].materialize.query_table('synapses_pni_2',filter_in_dict={'pre_pt_root_id':['%d'%cell_id]})
+    else: 
+        print("Did not recognize shape type! Should be one of : presynaptic, postsynaptic")
+      
+    print("This is the length of synapses: ", procObj['data_synapses'].shape)
+    
+    if cfg['startPSS'] > -1:
+        startpss = cfg['startPSS']
     else:
-        procObj['sk'] = skeleton_io.read_skeleton_h5(skelfile)
-    procObj['data_synapses'] = cfg['dl'].query_synapses('pni_synapses_i1', post_ids = [int(cell_id)])
-    cell_center_of_mass_list = cfg['neuron_df'][cfg['neuron_df']['soma_id']==int(cell_id)]['nucleus_center_mass_nm'].values
-    procObj['cell_center_of_mass'] = np.array(cell_center_of_mass_list[0])
-    procObj['rng'] =  range(0,len(procObj['data_synapses']))
+        startpss = 0
+    
+    if cfg['stopPSS'] > -1:
+        stoppss = cfg['stopPSS']
+    else:
+        stoppss = len(procObj['data_synapses'])
     
     
-    #process each pss
-    pss_extraction_utils.myParallelProcess(procObj)
+    if cfg['selectedPSS'] < 0:
+        procObj['rng'] =  range(startpss,stoppss)
     
-    #l = len(procObj['rng'])
-    #from multiprocessing import Pool
-    #from contextlib import closing
-    #with closing( Pool(20) ) as p:
-    #    partial_process = partial(myprocessingfunc,procObj,l)
-    #    rng = procObj['rng']
-    #    p.map(partial_process,rng)
+    else:
+        procObj['rng'] = range(cfg['selectedPSS'],cfg['selectedPSS']+1)
+        
+    print("settingup cfg")
+        
+    if cfg['type_of_shape'] == 'postsynaptic':
+        procObj['cell_center_of_mass'] =cfg['client'].materialize.query_table('nucleus_detection_v0',filter_in_dict={'pt_root_id':['%d'%cell_id]})['pt_position'].values[0]
+    else:
+        procObj['cell_center_of_mass'] = None
+        
+    print("done with query")
     
-    return procObj
+    #print(procObj['cell_center_of_mass'])
+    procObj['mesh_bounds'] = cfg['mesh_bounds']
+    
+    procObj['client'] = cfg['client']
+    procObj['forcerun'] = cfg['forcerun']
+    #procObj['cv'] = cfg['cv']
+    
+    #spinemesh= pss_extraction_utils_updated.myprocessingfunc(procObj,0,72)
+    #return spinemesh
+    print("Debug 0")
+    if run == "parallel" :
+        print("Doing parallel")
+        obj = pss_extraction_utils_updated.myParallelProcess(procObj)
+    elif run == "parallelTasks":
+        print("Doing parallel tasks")
+        obj = pss_extraction_utils_updated.myParallelTasks(procObj)
+    else:
+        print("Doing serial")
+        obj = pss_extraction_utils_updated.mySerialProcess(procObj)
+        
+    print("Debug 1 : ", type(obj))
+    
+    return procObj,obj
 
-def imyprocessingfunc (mynewdict,l,m):
-    print('m',mynewdict['cell_id'])
+
     
 def generate_features(procObj):
     procObj['pointnet_files'] = glob.glob(procObj['outdir']+'/PSS*.off')
     tf.reset_default_graph()
-    pss_extraction_utils.evaluate(procObj)
+    pss_extraction_utils_updated.evaluate(procObj)
     tf.reset_default_graph()
 
 
-def extract_shape_and_generate_features(config_file,cell_id_list = None):
+def extract_shape_and_generate_features_updated(config_file,cell_id_list,run="parallel"):
     with open(config_file) as f:
       cfg = json.load(f)
     
-    #INITS
-    cfg['dl'] = AnalysisDataLink(dataset_name=cfg['dataset_name'],
-                         sqlalchemy_database_uri=cfg['sqlalchemy_database_uri'],
-                         materialization_version=cfg['data_version'],
-                         verbose=False)
+    #framework client for database and cv conneciton for mesh downloads
+    cfg['client'] = FrameworkClient(cfg['dataset_name'],auth_token_file=cfg['auth_token_file'])
 
+    
+    #seg_source = cfg['client'].info.segmentation_source()
+    tokenfile = cfg['auth_token_file']
+    with open(tokenfile) as f:
+      tokencfg = json.load(f)
+    #cfg['cv'] = cloudvolume.CloudVolume(seg_source, use_https=True,secrets=tokencfg['token'])
+    
+    cfg['token'] = tokencfg['token']
+    cfg['segsource'] = cfg['client'].info.segmentation_source()
+    
+    #tensorflow variables
     cfg['tensorflow_model'] = importlib.import_module('models.model') # import network module
     if not os.path.exists(cfg['pointnet_dump_dir']): os.mkdir(cfg['pointnet_dump_dir'])
     cfg['LOG_FOUT'] = open(os.path.join(cfg['pointnet_dump_dir'], 'log_evaluate.txt'), 'w')
     
-    
-    #start processing
-    cfg['neuron_df'] = pd.read_pickle(cfg['input_cell_db'])
-    if (cell_id_list == None):
-        cell_id_list = list(neuron_df['soma_id'])
-        
+    #Processing
     for cell_id in cell_id_list:
-
-        procObj = extract_shape(cfg,cell_id)
+        print(cell_id)
+        procObj,obj = extract_shape_updated(cfg,cell_id,run)
         cfg['offfiles'] = procObj['offfiles']
         cfg['outdir'] = procObj['outdir']
         procObj = generate_features(cfg)
-
-
-def myprocessingfunc(Obj,l,q):
-    print ("%d out of %d "%(q,l))
-    
-    s = [4*Obj['data_synapses'].iloc[q]['ctr_pt_position'][0], 4*Obj['data_synapses'].iloc[q]['ctr_pt_position'][1], 40*Obj['data_synapses'].iloc[q]['ctr_pt_position'][2]]
-
-    print(s)
-    allmeshes, vertlabels,loc_mesh,pt,sdf,seg = pss_extraction_utils.get_segments_for_synapse(Obj,s)
-    
-    
-    time_start = time.time()
-    csg = loc_mesh._create_csgraph()
-    ccs = sparse.csgraph.connected_components(csg)
-    ccs_u, cc_sizes = np.unique(ccs[1], return_counts=True)
-    large_cc_ids = ccs_u[cc_sizes > 20]
-    etime = time.time()-time_start
-    
-    dist_to_center = np.linalg.norm(s-Obj['cell_center_of_mass'])
-    
-    print("Distance to center: ", dist_to_center, "cellid = ", Obj['cell_id'] )
-    
-    if (dist_to_center < 15000) & (np.max(cc_sizes) >5000):
         
-        spinemesh = pss_extraction_utils.create_closest_submesh(loc_mesh,seg,pt[0],pt[1],pt[2])
-        
-    else:
-                
-        try:
-            path = pss_extraction_utils.find_path_skel2synapse_cp(loc_mesh,sk,pt)
-        except:
-            path = None
-
-        if path is None:
-            spinemesh = pss_extraction_utils.create_closest_submesh(loc_mesh,seg,pt[0],pt[1],pt[2])
-        else:
-            start_time = time.time()
-            pathlabels = pss_extraction_utils.find_mesh_order(path,vertlabels)
-            
-            if len(pathlabels) > 1: #only cases with more than one segment (those will be either good ones or shafts)
-                sdf_verts = pss_extraction_utils.assign_labels_to_verts(loc_mesh,sdf)
-                sdf_mean = []
-                for ind in range(0,len(pathlabels)):
-                    lastmesh = allmeshes[pathlabels[ind]][0]
-                    t1 = pss_extraction_utils.get_indices_of_path(loc_mesh, lastmesh, path)
-                    sdfvec = [sdf_verts[t] for t in t1]
-                    sdf_mean.append(np.mean(sdfvec))
-
-                if sdf_mean[-1] > sdf_mean[-2]:
-                    pathlabels = pathlabels[:-1]
-            spinemeshes = [allmeshes[p] for p in pathlabels ]
-            
-            spinemesh = trimesh.util.concatenate(spinemeshes)
-            
-            elapsed_time = time.time() - start_time
-        
-    synapse_id = Obj['data_synapses'].iloc[q]['id']
-    trimesh.exchange.export.export_mesh(spinemesh, Obj['outdir'] + "/PSS_%d_%d.off"%(synapse_id,q))
-        
-    return spinemesh   
-
+    return procObj, obj
+    
+    
